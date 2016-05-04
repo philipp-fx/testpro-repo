32acff3e49048ed743c38bba4669dc9e357f519c
Philipp Seifert <philipp.seifert@iguanasell.com>
2016-05-04 12:49:18 +0200

    Comment 7 Mac

--- testpro-repo2Mac/testpro-repo2Mac/Form1.cs
+++ testpro-repo2Mac/testpro-repo2Mac/Form1.cs
@@ -26,6 +26,7 @@ namespace testpro_repo2Mac
             //Comment 4 WorkPC
             //Comment 5 WorkPC 04.05.2016 11:30h
             //Comment 6 Mac 04.05.2016 11:45h
+            //Comment 7 Mac 04.05.2016 12:48h
         }
     }
 }
